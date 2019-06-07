6e8502ee46164d232e0ac2c3f74a71d1d360e0d5
Royce Yang <royce1.yang@partner.midea.com>
2019-06-06 23:38:59 -0500

    Updates

---
+++ precache-manifest.f2dd092757d2bf1cc206e7b9e530d66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553bbe2c7ca53487248cfb91178f0990",
+    "url": "/cmsc-239-project-2/index.html"
+  },
+  {
+    "revision": "761e18af7466b4f7d5b9",
+    "url": "/cmsc-239-project-2/static/css/2.ebe653e1.chunk.css"
+  },
+  {
+    "revision": "6b1b69457a4e84e236b8",
+    "url": "/cmsc-239-project-2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "761e18af7466b4f7d5b9",
+    "url": "/cmsc-239-project-2/static/js/2.16f53a7f.chunk.js"
+  },
+  {
+    "revision": "6b1b69457a4e84e236b8",
+    "url": "/cmsc-239-project-2/static/js/main.becf57bd.chunk.js"
+  },
+  {
+    "revision": "049224b52142005c712c",
+    "url": "/cmsc-239-project-2/static/js/runtime~main.e89b2e80.js"
+  }
+]);
--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-./scripts/build-and-branch.sh
-git push --set-upstream origin gh-pages -f
-git add --a
-git stash
-git checkout master
